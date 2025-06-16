7b696451139efa6ec9cd94bce780ad477485f5ff
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-06-15 22:00:41 -0700

    polygon.js -- fix input tower

--- blocks_common/polygon.js
+++ blocks_common/polygon.js
@@ -1,8 +1,10 @@
 /**
  * @fileoverview Polygon block.
- * @author @RedMan13 (godslayerakp)
+ * @author @RedMan13 (godslayerakp) & SharkPool
  */
 'use strict';
+
+// TODO set the default value for checkboxes so the arrow doesnt point the wrong way on duplication/flyout click
 
 goog.provide('Blockly.Blocks.polygon');
 goog.require('Blockly.Blocks');
@@ -30,14 +32,15 @@ Blockly.Blocks['polygon'] = {
    * @this Blockly.Block
    */
   init: function() {
-    this.color = Blockly.Colours.pen.primary
-    this.expanded = true
-    this.points = 0
-    this.offset = [0,0]
-    this.scale = 50
-    this.oldConnections = {}
-    this.myBlocks = {}
-    this.generate()
+    this.color = Blockly.Colours.pen.primary;
+    this.needsInit = true;
+    this.expanded = true;
+    this.points = 0;
+    this.offset = [0,0];
+    this.scale = 50;
+    this.oldConnections = {};
+    this.myBlocks = {};
+    this.initGenerate();
   },
   mutationToDom: function() {
     const container = document.createElement('mutation');
@@ -47,67 +50,92 @@ Blockly.Blocks['polygon'] = {
     container.setAttribute('midle', JSON.stringify(this.offset));
     container.setAttribute('scale', JSON.stringify(this.scale));
     container.setAttribute('expanded', JSON.stringify(this.expanded));
+    container.setAttribute('needsInit', JSON.stringify(this.needsInit));
     return container;
   },
   domToMutation: function(xmlElement) {
-    const newPoints = JSON.parse(xmlElement.getAttribute('points'))
-    const newColor = xmlElement.getAttribute('color') || ''
-    const newOffset = JSON.parse(xmlElement.getAttribute('midle') || '""')
-    const newScale = JSON.parse(xmlElement.getAttribute('scale') || '""')
-    const newExpanded = JSON.parse(xmlElement.getAttribute('expanded') || 'false')
+    const newPoints = JSON.parse(xmlElement.getAttribute('points'));
+    const newColor = xmlElement.getAttribute('color') || '';
+    const newOffset = JSON.parse(xmlElement.getAttribute('midle') || '""');
+    const newScale = JSON.parse(xmlElement.getAttribute('scale') || '""');
+    const isInit = JSON.parse(xmlElement.getAttribute('needsInit') || 'true');
+    const newExpanded = false;
     if (newPoints !== this.points) {
-      this.clear()
-      this.points = newPoints
-      this.generate()
+      this.clear();
+      this.points = newPoints;
+      if (isInit) this.initGenerate();
+      else this.generate();
     }
     if (typeof newColor === 'string') {
-      this.color = newColor
+      this.color = newColor;
     }
     if (newOffset && Array.isArray(newOffset)) {
-      this.offset = newOffset
+      this.offset = newOffset;
     }
     if (typeof newScale === 'number') {
-      this.length = newScale
+      this.length = newScale;
     }
     if (typeof newExpanded === 'boolean' && newExpanded !== this.expanded) {
-      this.setExpanded(newExpanded)
-      this.setFieldValue(newExpanded, 'button')
+      this.setExpanded(newExpanded);
+      this.setFieldValue(newExpanded, 'button');
     }
   },
   clear: function() {
-    const connections = {}
+    const connections = {};
     for (let point = 1; point <= this.points; point++) {
-      const xName = `x${point}`
-      const yName = `y${point}`
-      const xInput = this.getInput(xName)
-      const yInput = this.getInput(yName)
-      connections[xName] = xInput.connection.targetConnection
-      connections[yName] = yInput.connection.targetConnection
-      this.removeInput(xName)
-      this.removeInput(yName)
+      const xName = `x${point}`;
+      const yName = `y${point}`;
+      const xInput = this.getInput(xName);
+      const yInput = this.getInput(yName);
+      connections[xName] = xInput.connection.targetConnection;
+      connections[yName] = yInput.connection.targetConnection;
+      this.removeInput(xName);
+      this.removeInput(yName);
     }
 
-    this.removeInput('buttonContainer')
-    this.oldConnections = connections
+    this.removeInput('buttonContainer');
+    this.oldConnections = connections;
+  },
+  initGenerate: function() {
+    this.setColour(this.color, this.color, this.color);
+    this.setOutputShape(Blockly.OUTPUT_SHAPE_SQUARE);
+    this.setOutput(true, 'math_polygon');
+    this.setShadow(true);
+
+    const thisBlock = this;
+    const button = new Blockly.FieldCheckbox(
+      this.expanded, 
+      newState => {
+        if (this.isInFlyout) return;
+        this.needsInit = false;
+        this.generate();
+        this.inputList[0].setVisible(false);
+        this.render();
+        return newState;
+      }
+    )
+    this.appendDummyInput('buttonContainer')
+      .appendField(button, 'button');
+    this.render(true);
   },
   generate: function() {
-    const connections = this.oldConnections
+    const connections = this.oldConnections;
     // create all the node inputs
     for (let point = 1; point <= this.points; point++) {
-      const xName = `x${point}`
-      const yName = `y${point}`
-      const xInput = this.appendValueInput(xName)
-      const yInput = this.appendValueInput(yName)
-      const xConnection = xInput.connection
-      const yConnection = yInput.connection
+      const xName = `x${point}`;
+      const yName = `y${point}`;
+      const xInput = this.appendValueInput(xName);
+      const yInput = this.appendValueInput(yName);
+      const xConnection = xInput.connection;
+      const yConnection = yInput.connection;
       // dispose of any free-floating blocks that where created by this block
       if (this.myBlocks[xName] && !this.myBlocks[xName].outputConnection.targetConnection) {
-        this.myBlocks[xName].dispose()
-        delete this.myBlocks[xName]
+        this.myBlocks[xName].dispose();
+        delete this.myBlocks[xName];
       }
       if (this.myBlocks[yName] && !this.myBlocks[yName].outputConnection.targetConnection) {
-        this.myBlocks[yName].dispose()
-        delete this.myBlocks[yName]
+        this.myBlocks[yName].dispose();
+        delete this.myBlocks[yName];
       }
       // if this point isnt filled in, fill it in
       if (!this.getInput(xName).connection.targetConnection || !this.getInput(yName).connection.targetConnection) {
@@ -118,58 +146,62 @@ Blockly.Blocks['polygon'] = {
         newyBlock.setFieldValue(String(initialValue[1]), 'NUM');
         newxBlock.setShadow(true);
         newyBlock.setShadow(true);
-        if (this.expanded){
-          newxBlock.initSvg();
-          newyBlock.initSvg();
-          newxBlock.render(false);
-          newyBlock.render(false);
-        }
-        newxBlock.outputConnection.connect(xConnection)
-        newyBlock.outputConnection.connect(yConnection)
-        this.myBlocks[xName] = newxBlock
-        this.myBlocks[yName] = newyBlock
+        newxBlock.initSvg();
+        newyBlock.initSvg();
+        newxBlock.render(true);
+        newyBlock.render(true);
+
+        newxBlock.outputConnection.connect(xConnection);
+        newyBlock.outputConnection.connect(yConnection);
+        this.myBlocks[xName] = newxBlock;
+        this.myBlocks[yName] = newyBlock;
       }
       // if we have a cached connection for this point then connect it
       if (connections[xName] || connections[yName]) {
-        const xBlock = connections[xName].getSourceBlock()
-        const yBlock = connections[yName].getSourceBlock()
-        connections[xName].connect(xConnection)
-        connections[yName].connect(yConnection)
+        const xBlock = connections[xName].getSourceBlock();
+        const yBlock = connections[yName].getSourceBlock();
+        connections[xName].connect(xConnection);
+        connections[yName].connect(yConnection);
         // re-render the blocks after connecting them
         xBlock.initSvg();
         yBlock.initSvg();
         xBlock.render(false);
         yBlock.render(false);
       }
-      xInput.appendField('x: ')
-      yInput.appendField('y: ')
+      xInput.appendField('x: ');
+      yInput.appendField('y: ');
     }
 
-    this.setColour(this.color, this.color, this.color)
-    this.setOutputShape(Blockly.OUTPUT_SHAPE_SQUARE)
-    this.setOutput(true, 'math_polygon')
+    this.setColour(this.color, this.color, this.color);
+    this.setOutputShape(Blockly.OUTPUT_SHAPE_SQUARE);
+    this.setOutput(true, 'math_polygon');
     this.setShadow(true);
 
     const thisBlock = this;
     const button = new Blockly.FieldCheckbox(
       this.expanded, 
       newState => {
-        thisBlock.setExpanded(newState)
-        return newState
+        this.needsInit = false;
+        thisBlock.setExpanded(newState);
+        return newState;
       }
     )
     this.appendDummyInput('buttonContainer')
-      .appendField(button, 'button')
+      .appendField(button, 'button');
+    this.render(true);
   },
   setExpanded: function(bool) {
-    this.expanded = bool
+    if (this.needsInit) return;
+    this.expanded = bool;
     for (let point = 1; point <= this.points; point++) {
-      const xName = `x${point}`
-      const yName = `y${point}`
-      const xInput = this.getInput(xName)
-      const yInput = this.getInput(yName)
-      xInput.setVisible(bool)
-      yInput.setVisible(bool)
+      const xName = `x${point}`;
+      const yName = `y${point}`;
+      const xInput = this.getInput(xName);
+      const yInput = this.getInput(yName);
+      xInput.visible_ = !bool;
+      xInput.setVisible(bool);
+      yInput.visible_ = !bool;
+      yInput.setVisible(bool);
     }
     this.initSvg();
     // we dont need to re-render this block since renderChildren will render all parents aswell
@@ -177,14 +209,14 @@ Blockly.Blocks['polygon'] = {
   },
   rerenderChildBlocks: function() {
     const renderInputs = (block) => {
-      const children = block.childBlocks_
+      const children = block.childBlocks_;
       // once we hit a bottom block, rerender the whole tree
-      if (children.length) block.render(true)
+      if (children.length) block.render(true);
       for (var i = 0, child; child = children[i]; i++) {
-        child.render(false)
-        renderInputs(child)
+        child.render(false);
+        renderInputs(child);
       }
     }
-    renderInputs(this)
+    renderInputs(this);
   }
 };
