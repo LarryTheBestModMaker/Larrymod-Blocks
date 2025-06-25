24ada215443f226fb70e600df7500d95bdcc2278
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-06-25 20:50:06 +0200

    fix

--- core/data_category.js
+++ core/data_category.js
@@ -431,9 +431,10 @@ Blockly.VariableCategory.addCreateButton = function(xmlList, workspace, type) {
   var callbackKey = 'CREATE_VARIABLE';
   var callback = function(button) {
     const workspace = button.getTargetWorkspace();
-    Blockly.Variables.createVariable(workspace, null, '');};
+    Blockly.Variables.createVariable(workspace, null, '');
     const updatedToolbox = workspace.options.languageTree.cloneNode(true)
     workspace.updateToolbox(updatedToolbox);
+  };
 
   if (type === 'LIST') {
     msg = Blockly.Msg.NEW_LIST;
@@ -441,9 +442,10 @@ Blockly.VariableCategory.addCreateButton = function(xmlList, workspace, type) {
     callback = function(button) {
       const workspace = button.getTargetWorkspace();
       Blockly.Variables.createVariable(workspace, null,
-          Blockly.LIST_VARIABLE_TYPE);};
+          Blockly.LIST_VARIABLE_TYPE);
       const updatedToolbox = workspace.options.languageTree.cloneNode(true)
       workspace.updateToolbox(updatedToolbox);
+    };
   }
   button.setAttribute('text', msg);
   button.setAttribute('callbackKey', callbackKey);
