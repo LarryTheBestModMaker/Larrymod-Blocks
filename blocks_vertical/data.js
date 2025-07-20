b0627c3d2f014861353f9dfbc1005581ced73c88
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-07-20 17:52:18 +0200

    test

--- blocks_vertical/data.js
+++ blocks_vertical/data.js
@@ -385,17 +385,21 @@ Blockly.Blocks['data_replaceitemoflist'] = {
 };
 
 Blockly.Blocks['data_itemoflist'] = {
-  /**
+  /** 
    * Block for reporting item of list.
    * @this Blockly.Block
-   */
+  */
   init: function() {
     this.jsonInit({
       "message0": Blockly.Msg.DATA_ITEMOFLIST,
       "args0": [
         {
-          "type": "input_value",
-          "name": "INDEX"
+          "type": "field_textdropdown",
+          "name": "INDEX",
+          "options": [
+            [ "last", "last" ],
+            [ "random", "random" ],
+          ]
         },
         {
           "type": "field_variable",
