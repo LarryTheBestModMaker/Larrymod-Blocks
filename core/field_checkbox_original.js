06972fd18098c45622a77f78bc8ea68513c7e118
jwklong <jwklong@gmx.com>
2025-08-25 11:10:27 +0000

    reorder code because its acting weird

--- core/field_checkbox_original.js
+++ core/field_checkbox_original.js
@@ -34,12 +34,12 @@ const updateCheckColor = (field) => {
   const srcBlock = field.sourceBlock_;
   if (!srcBlock) return;
   if (field.state_ == "TRUE") srcBlock.setShadowColour("#33D833");
-  if (field.checkBackground_) field.checkBackground_.setAttribute('fill', field.state_ == "TRUE" ? "#33D833" : srcBlock.getColourTertiary())
   else if (srcBlock.parentBlock_) {
     srcBlock.shadowColour_ = "#00000035";
     srcBlock.updateColour();
     if (srcBlock.svgPath_) srcBlock.svgPath_.setAttribute("stroke", "#00000000");
   }
+  if (field.checkBackground_) field.checkBackground_.setAttribute('fill', field.state_ == "TRUE" ? "#33D833" : srcBlock.getColourTertiary());
 };
 
 /**
