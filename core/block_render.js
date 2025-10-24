e96cfd8f791f56aa57c7c3d046f624cf6ea0f824
jwklong <72522395+jwklong@users.noreply.github.com>
2025-10-24 16:40:54 +0100

    w rizz

--- core/block_render.js
+++ core/block_render.js
@@ -2294,11 +2294,11 @@ Blockly.BlockSvg.CUSTOM_SHAPES = new Map([
         emptyInputWidth: 12 * Blockly.BlockSvg.GRID_UNIT,
         leftPath: (block) => {
             const scale = block.height / 2;
-            return [`h ${-scale} l ${scale} ${-scale} l ${-scale} ${-scale} h ${scale}`];
+            return [`h ${-scale * 2} l ${scale} ${-scale} l ${-scale} ${-scale} h ${scale * 2}`];
         },
         rightPath: (block) => {
             const scale = block.edgeShapeWidth_;
-            return [`h ${scale} l ${-scale} ${scale} l ${scale} ${scale} h ${-scale}`];
+            return [`h ${scale * 2} l ${-scale} ${scale} l ${scale} ${scale} h ${-scale * 2}`];
         },
         /*blockPaddingStart: (_, __, firstInput) => {
             return Math.max(((firstInput.renderHeight - Blockly.BlockSvg.MIN_BLOCK_Y_REPORTER)) / 2, 0) + 4;
