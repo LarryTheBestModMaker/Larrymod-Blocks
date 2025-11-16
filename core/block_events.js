093033caed8d5e3166b931edffda0b8c1c51af6e
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-11-16 14:13:53 -0800

    block_events.js -- expandable event here too

--- core/block_events.js
+++ core/block_events.js
@@ -386,6 +386,7 @@ Blockly.Events.Move = function(block) {
   this.oldParentId = location.parentId;
   this.oldInputName = location.inputName;
   this.oldCoordinate = location.coordinate;
+  this.isFromExpandable = Blockly.Events.expandableClick;
 };
 goog.inherits(Blockly.Events.Move, Blockly.Events.BlockBase);
 
