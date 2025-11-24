8f6885c8fdfbd41d59da7a2c2ca9f57f0a748f75
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-11-24 16:08:15 +0100

    ?

--- core/data_category.js
+++ core/data_category.js
@@ -130,9 +130,8 @@ Blockly.VariableCategory.ListCategory = function(workspace) {
     Blockly.VariableCategory.addBlock(xmlList, firstList, 'data_filterlist', 'LIST', [
         ['INDEX', 'data_filterlistindex'], ['ITEM', 'data_filterlistitem'], ['BOOL', 'checkbox']
     ]);
-    Blockly.VariableCategory.addBlock(xmlList, firstList, 'data_arraylist', 'LIST', [
-        ['VALUE', 'text', '["a", "b", "c"]']
-    ]);
+    Blockly.VariableCategory.addBlock(xmlList, firstList, 'data_arraylist',
+        'LIST', ['VALUE', 'text', '["a", "b", "c"]']);
     Blockly.VariableCategory.addBlock(xmlList, firstList, 'data_listarray', 'LIST');
     Blockly.VariableCategory.addSep(xmlList);
     Blockly.VariableCategory.addShowList(xmlList, firstList);
