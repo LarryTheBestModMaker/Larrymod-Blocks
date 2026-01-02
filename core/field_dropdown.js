0f4ee63521e351c718333f3b21f8a8c6ca144d6f
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-01-02 12:55:20 -0800

    set text color after render

--- core/field_dropdown.js
+++ core/field_dropdown.js
@@ -205,11 +205,11 @@ Blockly.FieldDropdown.prototype.showEditor_ = function() {
     menuItem.setRightToLeft(this.sourceBlock_.RTL);
     menuItem.setValue(value);
     menuItem.setCheckable(true);
+    menu.addChild(menuItem, true);
     if (this.sourceBlock_.textColour) {
       menuItem.element_.style.color = this.sourceBlock_.textColour;
     }
 
-    menu.addChild(menuItem, true);
     var checked = (value == this.value_);
     menuItem.setChecked(checked);
     if (checked) {
